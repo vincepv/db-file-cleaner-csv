737f53303a612f04435324f6723c44742be6bf3a
vpv <support@digitalebox.fr>
2025-11-03 11:36:11 +0100

    fix categorie enum

--- components/category_create.py
+++ components/category_create.py
@@ -2,6 +2,6 @@ from constant.column_name import CATEGORIE
 
 def category_create(df):
     if CATEGORIE not in df.columns:
-        df[CATEGORIE] = 'prospect'
+        df[CATEGORIE] = '2'
     
     return df
